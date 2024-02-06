3c2a353fb84cfc2d1776d7f93a8e4af7bd4d4444
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2024-02-06 23:39:49 +0300

    Version 1.4.4.
    New export feature added! Now you can export your masterpiece to .txt file. Now it supports only one file at the same time (will be improved). Some other little improvements

--- main.asm
+++ main.asm
@@ -118,6 +118,7 @@ SetWindowSize endp
 
 DrawCell proc uses ebx ecx esi edi hOut: DWORD, dwCoord: DWORD
 	
+	invoke SetColor, drawColor
 	lea esi, szBrushBuffer
 	mov ebx, 0
 	
@@ -181,64 +182,64 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 			
 			; FIRST ROW OF BRUSHES
 				
-			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick			
 				mov byte ptr[szToDraw], exclBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], quoteBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], sharpBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], dollarBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], comAndBrush	
 				
-			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 3 && bx < 6
+			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx >= 3 && bx < 6
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], zeroQuoteBrush	
 			
 			; SECOND ROW OF BRUSHES
 				
-			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx >= 6 && bx < 9
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], parenthOBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx >= 6 && bx < 9
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], parenthCBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx >= 6 && bx < 9
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], multiplBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx >= 6 && bx < 9
 			
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				mov byte ptr[szToDraw], plusBrush
 				
-			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx >= 6 && bx < 9
 			
 				mov byte ptr[szToDraw], commaBrush	
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 6 && bx < 9
+			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx >= 6 && bx < 9
 			
 				mov byte ptr[szToDraw], minusBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
@@ -246,32 +247,32 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 				
 			; THIRD ROW OF BRUSHES
 			
-			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], dotBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], slashBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], colonBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], semicolonBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], lessBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
 				
-			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 9 && bx < 12
+			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx >= 9 && bx < 12
 			
 				mov byte ptr[szToDraw], equalBrush	
 				invoke PlaySoundOnClick, offset szPlayOnClick			
@@ -279,32 +280,32 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 			
 			; FOURTH ROW OF BRUSHES
 		
-			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], moreBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], questionBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], atBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], squareOBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], backSlashBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
 				
-			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 12 && bx < 15
+			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx >= 12 && bx < 15
 			
 				mov byte ptr[szToDraw], squareCBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
@@ -312,32 +313,32 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 			
 			; FIFTH ROW OF BRUSHES
 		
-			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+6 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], birdBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+8 && ax <= WORKING_AREA_WIDTH+11 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], traitBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+13 && ax <= WORKING_AREA_WIDTH+16 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], braceOBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], directBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick
 				
-			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], braceCBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
 				
-			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 15 && bx < 18
+			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx >= 15 && bx < 18
 			
 				mov byte ptr[szToDraw], tildaBrush
 				invoke PlaySoundOnClick, offset szPlayOnClick	
@@ -348,38 +349,47 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 20 && bx < 23
 			
 				mov byte ptr[drawSize], 1
+				invoke PlaySoundOnClick, offset szPlayOnClick
 				
 			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 20 && bx < 23
 			
-				mov byte ptr[drawSize], 2	
+				mov byte ptr[drawSize], 2
+				invoke PlaySoundOnClick, offset szPlayOnClick	
 				
 			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 20 && bx < 23
 			
 				mov byte ptr[drawSize], 3
+				invoke PlaySoundOnClick, offset szPlayOnClick
 				
 			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 23 && bx < 26
 			
 				mov byte ptr[drawSize], 4
+				invoke PlaySoundOnClick, offset szPlayOnClick
 				
 			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 23 && bx < 26
 			
-				mov byte ptr[drawSize], 5	
+				mov byte ptr[drawSize], 5
+				invoke PlaySoundOnClick, offset szPlayOnClick	
 				
 			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 23 && bx < 26
 			
 				mov byte ptr[drawSize], 6
+				invoke PlaySoundOnClick, offset szPlayOnClick
 				
 			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+21 && bx > 26 && bx < 29
 			
 				mov byte ptr[drawSize], 7
+				invoke PlaySoundOnClick, offset szPlayOnClick
 								
 			.elseif ax >= WORKING_AREA_WIDTH+23 && ax <= WORKING_AREA_WIDTH+26 && bx > 26 && bx < 29
 			
 				mov byte ptr[drawSize], 8
+				invoke PlaySoundOnClick, offset szPlayOnClick
 								
 			.elseif ax >= WORKING_AREA_WIDTH+28 && ax <= WORKING_AREA_WIDTH+31 && bx > 26 && bx < 29
 			
 				mov byte ptr[drawSize], 8
+				invoke PlaySoundOnClick, offset szPlayOnClick
 				
 			
 			; <<< ---------   COLOR BUTTONS CHECK   ------------- >>>
@@ -460,17 +470,28 @@ KeyController proc uses ebx ecx esi edi hIn: DWORD, hOut: DWORD
 			; EXPORT
 			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+31 && bx > WORKING_AREA_HEIGHT-3 && bx < WORKING_AREA_HEIGHT+1
 			
+				invoke ExportImageEvent
 				invoke PlaySoundOnClick, offset szPlayOnClick
 			
+				invoke PutCursorToPos, 1, WORKING_AREA_HEIGHT+3
+				
+				invoke SetColor, LightGray
+				fn crt_printf, "Picture saved to image.txt. "
+				
+				invoke SetColor, cRed
+				fn crt_printf, "Be Careful, you can save ONLY 1 picture at the moment"
+				
 			; IMPORT	
 			.elseif ax >= WORKING_AREA_WIDTH+18 && ax <= WORKING_AREA_WIDTH+31 && bx > WORKING_AREA_HEIGHT-6 && bx < WORKING_AREA_HEIGHT-3
 			
+				invoke ImportImageEvent
 				invoke PlaySoundOnClick, offset szPlayOnClick
 			
 			; ERASER	
 			.elseif ax >= WORKING_AREA_WIDTH+3 && ax <= WORKING_AREA_WIDTH+9 && bx >= WORKING_AREA_HEIGHT-6 && bx < WORKING_AREA_HEIGHT-3
 				
-				mov byte ptr[szToDraw], eraseBrush
+				;mov byte ptr[szToDraw], eraseBrush
+				invoke SetColor, 0
 				invoke PlaySoundOnClick, offset szPlayOnClick
 					
 			.endif
@@ -491,5 +512,46 @@ SetColor proc uses ebx esi edi color: DWORD
 
 	Ret
 SetColor endp
+
+ExportImageEvent proc uses ebx esi edi
+
+	LOCAL hFileExport: DWORD
+	LOCAL nRead: DWORD
+	LOCAL hOut: DWORD
+
+	invoke CreateFile, offset szImage, GENERIC_WRITE, FILE_SHARE_WRITE, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+	mov hFileExport, eax
+	
+	mov ebx, 030001h
+	
+	
+	invoke GetStdHandle, STD_OUTPUT_HANDLE
+	mov hOut, eax
+	
+	fn ReadConsoleOutputCharacter, hOut, offset szBuffer2, WORKING_AREA_WIDTH, ebx, addr nRead
+	invoke lstrcat, offset szBuffer2, offset szNewLine
+	invoke WriteFile, hFileExport, offset szBuffer2, WORKING_AREA_WIDTH+2, addr nRead, 0
+	add ebx, 010000h
+	
+	.while (ebx < 0290001h)
+		
+		fn ReadConsoleOutputCharacter, hOut, offset szBuffer2, WORKING_AREA_WIDTH, ebx, addr nRead
+		
+		invoke WriteFile, hFileExport, offset szBuffer2, WORKING_AREA_WIDTH+2, addr nRead, 0
+	
+		add ebx, 010000h
+	.endw
+	
+	invoke CloseHandle, hFileExport
+	
+	Ret
+ExportImageEvent endp
+
+ImportImageEvent proc uses ebx esi edi
+
+
+
+	Ret
+ImportImageEvent endp
 
 end start
